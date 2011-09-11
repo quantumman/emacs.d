30ba4d9bec7f89a381cca2b378b73ce88e26c6fd
Shoadi Yokoyama <quantumcars@gmail.com>
2011-09-12 01:06:33 +0900

    Adopt a part of the default configuration of eshll
    because previous settings makes the screen be upset: it's flicking,
    and the cursor is nerver forcused on the latest prompt after commands
    executed.

--- init.d/init.eshell.el
+++ init.d/init.eshell.el
@@ -1,20 +1,26 @@
 (require 'eshell)
 (require 'env-test)
 
-
 (custom-set-variables
- '(eshell-ask-to-save-history (quote always))
- '(eshell-history-size 1000)
- '(eshell-ls-dired-initial-args (quote ("-h")))
- '(eshell-ls-exclude-regexp "~\\'")
- '(eshell-ls-initial-args "-h")
- '(eshell-ls-use-in-dired t nil (em-ls))
- '(eshell-modules-list (quote (eshell-alias eshell-basic
-                                           eshell-cmpl eshell-dirs eshell-glob
-                                           eshell-hist eshell-ls eshell-pred
-                                           eshell-prompt eshell-rebind
-                                           eshell-script eshell-smart
-                                           eshell-term eshell-unix eshell-xtra)))
+ ;; '(eshell-ask-to-save-history (quote always))
+ ;; '(eshell-history-size 1000)
+ ;; '(eshell-ls-dired-initial-args (quote ("-h")))
+ ;; '(eshell-ls-exclude-regexp "~\\'")
+ ;; '(eshell-ls-initial-args "-h")
+ ;; '(eshell-ls-use-in-dired t nil (em-ls))
+ ;; '(eshell-modules-list (quote (eshell-alias eshell-basic
+ ;;                                           eshell-cmpl eshell-dirs eshell-glob
+ ;;                                           eshell-hist eshell-ls eshell-pred
+ ;;                                           eshell-prompt eshell-rebind
+ ;;                                           eshell-script eshell-smart
+ ;;                                           eshell-term eshell-unix eshell-xtra)))
+ '(eshell-output-filter-functions
+   '(eshell-handle-control-codes
+     eshell-watch-for-password-prompt
+     eshell-postoutput-scroll-to-bottom
+     eshell-handle-ansi-color))
+ '(eshell-scroll-show-maximum-output t)
+ '(eshell-scroll-to-bottom-on-output nil)
  '(eshell-prefer-to-shell t nil (eshell))
  '(eshell-stringify-t nil)
  '(eshell-term-name "ansi")
