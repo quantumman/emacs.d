29927649f7b15d1ebbf22c3df9c55eb022c4fad5
Shoadi Yokoyama <quantumcars@gmail.com>
2011-12-15 09:24:38 +0900

    add config for test

---
+++ emacs-test-init.el
@@ -0,0 +1,12 @@
+;;; less is more
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;;; show testing on the frame title
+(setq frame-title-format "emacs-test")
+
+;;; run debugger if error occurred
+(setq debug-on-error)
+
+
