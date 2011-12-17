02064890673e07c548b6658cf9dc3e2b9a18bd38
Shoadi Yokoyama <quantumcars@gmail.com>
2011-12-17 21:17:09 +0900

    Add a new function killing region or running ispell.
    That does s kill-region if region is activate,
    and checks spell using ispell if region is deactive.

--- init.d/init.custom-key-binding.el
+++ init.d/init.custom-key-binding.el
@@ -13,6 +13,20 @@
   (move-to-window-line -1))
 (global-set-key "\C-b" 'point-to-bottom) ;; C-b = pointer moves to bottom of window
 
+(defun spell-checking-or-kill-region ()
+  "Run spell-checking if there is no marker,
+run kill-region if marker and current point are different position."
+  (interactive)
+  (let ((start (mark-marker))
+	(end (point-marker)))
+    (if (region-active-p)
+	(progn
+	  (kill-region start end)
+	  (deactivate-mark))
+	(ispell-word)
+	)))
+(global-set-key "\C-w" 'spell-checking-or-kill-region)
+
 (global-set-key "\C-f" 'forward-word)
 (global-set-key "\C-b" 'backward-word)
 
