6224c88af9077fe7f828a82fcfdadce63a4bf965
Shoadi Yokoyama <quantumcars@gmail.com>
2011-12-17 17:17:25 +0900

    Describe sequential commnads

--- init.d/init.sequential-command.el
+++ init.d/init.sequential-command.el
@@ -1,10 +1,13 @@
 ;;;; sequential command
 (require 'sequential-command)
+
+;; head of sentence -> head of line -> beginning of buffer -> departure
 (define-sequential-command seq-home
   back-to-indentation beginning-of-line point-to-top
   beginning-of-buffer seq-return)
 (global-set-key "\C-a" 'seq-home)
 
+;; end of line -> end of buffer -> departure
 (define-sequential-command seq-end
   end-of-line point-to-bottom end-of-buffer seq-return)
 (global-set-key "\C-e" 'seq-end)
