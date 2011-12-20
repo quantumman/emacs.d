22944644389d730b9aae97b48aac2fa5c9c64d1c
Shoadi Yokoyama <quantumcars@gmail.com>
2011-12-21 03:23:15 +0900

    change, add and remove many things many times

--- test/anything-c-source-windows.el
+++ test/anything-c-source-windows.el
@@ -1,34 +1,38 @@
-(setq anything-c-source-windows
-      '((name . "Other windoes")
-        (candidates . (lambda ()
-                        (loop for buffer-list in (get-buffer-lists)
-                              for window = (char-to-string (+ ?` (car buffer-list)))
-                              for buffers = (cdr buffer-list)
-                              collect(format "%s [%%s]" window buffers)
-                              )
-                        )
-        )
-      )
+(defvar anything-c-source-windows
+      '((name . "Other windows")
+        (candidates . anything-window-candinates)
+        (action . (lambda (window)
+                    (let* ((to-window (car (split-string window " ")))
+                           (window-id (- (string-to-char to-window) ?`))
+                           (action (cdr (anything-window-actions to-window))))
+                      (win:save-window win:current-config)
+                      (with-temp-buffer
+                        (win-switch-to-window 1 window-id)
+                        (when action (funcall action)))
+                      )))))
 
-(aref win:names-prefix 1)
+(defun anything-window-candinates ()
+  (loop for buffer-list in (get-buffer-lists)
+        for window = (char-to-string (+ ?` (car buffer-list)))
+        for buffers = (cdr buffer-list)
+        collect (format "%s [%s]" (car (anything-window-actions window))
+                        (mapconcat #'identity buffers " "))))
+
+(defun anything-window-actions (w)
+  (let ((action (cdr (assoc w anything-window-config))))
+    (if action action (list w))))
+
+(setq anything-window-config
+  '(
+    ("t" "twitter" #'twit)
+    ("o" "org" #'(lambda () (unless (eq major-mode 'org-mode) (princ "open-org-mode")))))
+  )
 
 (require 'cl)
 (defun get-buffer-lists ()
   (loop for i from 1 to (- win:max-configs 1)
         for buffers = (mapcar #'string-trim (split-string (aref win:names i) "/"))
-        do (win:store-config i)
         collect (cons i buffers)))
 
 (defun string-trim (str)
   (replace-regexp-in-string "^\\s-+\\|\\s-+$" "" str))
-
-(car (assoc 2 (get-buffer-lists)))
-(string-trim "hoge ")
-
-(aref win:names 1)
-(win:store-config 1)
-(aref win:configs 0)
-
-(aref win:buflists 3)
-
-(win:current-config)
