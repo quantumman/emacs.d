091b468f05001b866299718adf8fc8addde47310
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2012-08-03 15:25:29 +0900

    Use anything.el and releated elisps distributed from emacswiki

--- elpa/anything-el-swank-fuzzy-0.1.2009/anything-el-swank-fuzzy-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; anything-el-swank-fuzzy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("anything-el-swank-fuzzy-pkg.el" "anything-el-swank-fuzzy.el")
-;;;;;;  (20176 39475 709058))
-
-;;;***
-
-(provide 'anything-el-swank-fuzzy-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; anything-el-swank-fuzzy-autoloads.el ends here
