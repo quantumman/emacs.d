59f5f8b942c25a3a0d1e8977ed22fab883df42fd
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2012-08-10 10:52:33 +0900

    Add melpa to package repository

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
       (load (expand-file-name "~/.emacs.d/elpa/package.el")))
   (package-initialize))
 (require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
