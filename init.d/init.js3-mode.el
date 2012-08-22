53c221e2e8661b9f88cf3cd061197d2e8c2ac87e
Shoadi Yokoyama <quantumcars@gmail.com>
2012-08-23 07:46:47 +0900

    Add javascript reserved words list for auto-completion

---
+++ init.d/init.js3-mode.el
@@ -0,0 +1,6 @@
+(defvar ac-source-javascript-reserved-words
+  '((candidates  . (list "break" "case" "catch" "continue" "debugger" "default" "delete"
+                         "do" "else" "finally" "for" "function" "if" "in" "instanceof"
+                         "new" "return"  "switch"  "this" "throw" "throw" "try"
+                         "typeof" "var" "void" "while" "with"))
+    (cache)))
