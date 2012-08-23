c27f45a3223b00d08079f5b40811ce76d30c4012
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2012-08-23 10:00:40 +0900

    Show tab spaces with color

--- init.d/init.appearance.el
+++ init.d/init.appearance.el
@@ -185,10 +185,30 @@
 		("-cdac$" . 1.3)))
     )
 
-  (global-whitespace-mode 1)
+  (require 'whitespace)
 
   ;; スペースの定義は全角スペースとする。
-  (setq whitespace-space-regexp "\x3000+")
+  (setq whitespace-spacge-regexp "\x3000+")
+
+  (setq whitespace-style '(face tabs tab-mark spaces space-mark))
+
+  (setq whitespace-display-mappings
+	'((space-mark ?\u3000 [?\□])
+	  (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])
+	  (newline-mark ?\n [?\u21B5 ?\n] [?$ ?\n])))
+
+  ;; 半角スペースを除外
+  ;; (dolist (d '((space-mark ?\ ))
+  ;;   (setq whitespace-display-mappings
+  ;; 	  (delete-if
+  ;; 	   '(lambda (e) (and (eq (car d) (car e))
+  ;; 			     (eq (cadr d) (cadr e))))
+  ;; 	   whitespace-display-mappings))))
+
+  ;; 強調したくない要素を削除
+  ;; (dolist (d '(face lines space-before-tab
+  ;;       	    indentation empty space-after-tab tab-mark))
+  ;;   (setq whitespace-style (delq d whitespace-style)))
 
   ;; 改行の色を変更
   (set-face-foreground 'whitespace-newline "gray60")
@@ -202,24 +222,7 @@
   (set-face-background 'whitespace-tab nil)
   (set-face-underline 'whitespace-tab "grey80")
 
-
-  ;; 半角スペースと改行を除外
-  (dolist (d '((space-mark ?\ ) (newline-mark ?\n)))
-    (setq whitespace-display-mappings
-	  (delete-if
-	   '(lambda (e) (and (eq (car d) (car e))
-			     (eq (cadr d) (cadr e))))
-	   whitespace-display-mappings)))
-
-  ;; 全角スペースと改行を追加
-  (dolist (e '((space-mark   ?\x3000 [?\□])
-	       (newline-mark ?\n     [?\u21B5 ?\n] [?$ ?\n])))
-    (add-to-list 'whitespace-display-mappings e))
-
-  ;; 強調したくない要素を削除
-  (dolist (d '(face lines space-before-tab
-		    indentation empty space-after-tab tab-mark))
-    (setq whitespace-style (delq d whitespace-style)))
+  (global-whitespace-mode t)
 )
 
 (when (= emacs-major-version 22)
