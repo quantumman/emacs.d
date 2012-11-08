35f1eac28202b350ac596973e81c8a1ac3cd2f36
Shoadi Yokoyama <quantumcars@gmail.com>
2012-11-09 00:43:06 +0900

    init.appearance: Rewrite font config

--- init.d/init.appearance.el
+++ init.d/init.appearance.el
@@ -75,40 +75,31 @@
     (setq fixed-width-use-QuickDraw-for-ascii t)
     (setq mac-allow-anti-aliasing t)
     (add-to-list 'default-frame-alist '(font . "fontset-default"))
-
-    ;; フォントセットを作る
-    (let* ((fontset-name "default") ; フォントセットの名前
-           (size 13) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
-           (asciifont "Monaco") ; ASCIIフォント
-           (jpfont "Hiragino Maru Gothic ProN") ; 日本語フォント
-           (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
-           (fontspec (font-spec :family asciifont))
-           (jp-fontspec (font-spec :family jpfont))
-           (fsn (create-fontset-from-ascii-font font nil fontset-name)))
-      (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
-      (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
-      (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
-      (set-fontset-font fsn '(#x0080 . #x024F) fontspec) ; 分音符付きラテン
-      (set-fontset-font fsn '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
-      )
-
-    ;; デフォルトのフレームパラメータでフォントセットを指定
-    (add-to-list 'default-frame-alist '(font . "fontset-default"))
-
-    ;; フォントサイズの比を設定
-    (dolist (elt '(("^-apple-hiragino.*" . 1.2)
-                   (".*osaka-bold.*" . 1.0)
-                   (".*osaka-medium.*" . 1.0)
-                   (".*courier-bold-.*-mac-roman" . 1.0)
-                   (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-                   (".*monaco-bold-.*-mac-roman" . 0.9)))
-      (add-to-list 'face-font-rescale-alist elt))
-
-    ;; デフォルトフェイスにフォントセットを設定
-    ;; # これは起動時に default-frame-alist に従ったフレームが
-    ;; # 作成されない現象への対処
-    (set-face-font 'default "fontset-default"))
-
+    
+    (set-face-attribute 'default nil
+                        :family "monaco"
+                        :height 140)
+    (set-fontset-font
+     (frame-parameter nil 'font)
+     'japanese-jisx0208
+     '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+    (set-fontset-font
+     (frame-parameter nil 'font)
+     'japanese-jisx0212
+     '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+    (set-fontset-font
+     (frame-parameter nil 'font)
+     'mule-unicode-0100-24ff
+     '("monaco" . "iso10646-1"))
+    (setq face-font-rescale-alist
+          '(("^-apple-hiragino.*" . 1.2)
+            (".*osaka-bold.*" . 1.2)
+            (".*osaka-medium.*" . 1.2)
+            (".*courier-bold-.*-mac-roman" . 1.0)
+            (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+            (".*monaco-bold-.*-mac-roman" . 0.9)
+            ("-cdac$" . 1.3)))
+    )
 
   (when (or cygwin-p windows-p)
     ;; @ coding system
