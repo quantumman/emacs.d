28fcb74fc663d778c8dd640c657e1bb5aae458a9
Shoadi Yokoyama <quantumcars@gmail.com>
2012-11-29 23:12:11 +0900

    Use no symbol font for haskell-mode

--- init.d/init.haskell-mode.el
+++ init.d/init.haskell-mode.el
@@ -5,7 +5,7 @@
 ;; (remove-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;; (remove-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(setq haskell-font-lock-symbols t)
+(setq haskell-font-lock-symbols nil)
 (require 'inf-haskell)
 ;; (setq haskell-program-name "/usr/bin/ghci -XTemplateHaskell")
 (setq haskell-program-name "ghci -XTemplateHaskell")
