bdf0c47df206e60bd16009d78431c440c453760f
Shoadi Yokoyama <quantumcars@gmail.com>
2012-12-12 23:40:26 +0900

    init.ghc-mod: Refactor - Unify indentation

--- init.d/init.ghc-mod.el
+++ init.d/init.ghc-mod.el
@@ -2,15 +2,15 @@
 (require 'ghc)
 ;;;; ghc-mod
 (add-to-list 'exec-path
-	     (expand-file-name "~/.cabal/bin"))
+             (expand-file-name "~/.cabal/bin"))
 
 (autoload 'ghc-init "ghc" nil t)
 
 (add-hook 'haskell-mode-hook (lambda ()
-			       (ghc-init)
-			       ;; (flymake-mode)
-			       ;; (setq auto-save-buffers-exclude-regexp "\\.hs$")
-			       ))
+                               (ghc-init)
+                               ;; (flymake-mode)
+                               ;; (setq auto-save-buffers-exclude-regexp "\\.hs$")
+                               ))
 
 
 (defvar ac-source-ghc-completion
@@ -27,43 +27,43 @@
 
 ;; add some symbols into ac-sources
 (add-hook 'haskell-mode-hook
-	  '(lambda ()
-	     (auto-complete-mode t)
-	     (setq ac-sources
-		   '(ac-source-ghc-completion
-		     ;; ac-source-symbols
-		     ac-source-ghc-module
-		     ;; ac-source-ghc-pragmas
-		     ;; ac-source-ghc-langexts
-		     ac-source-yasnippet
-		     ac-source-words-in-same-mode-buffers
-		     ac-source-abbrev))
-	     nil ))
+          '(lambda ()
+             (auto-complete-mode t)
+             (setq ac-sources
+                   '(ac-source-ghc-completion
+                     ;; ac-source-symbols
+                     ac-source-ghc-module
+                     ;; ac-source-ghc-pragmas
+                     ;; ac-source-ghc-langexts
+                     ac-source-yasnippet
+                     ac-source-words-in-same-mode-buffers
+                     ac-source-abbrev))
+             nil ))
 
 
 (defun ghc-flymake-display-errors-popup ()
   (interactive)
   (when (and (ghc-flymake-have-errs-p)
-	     (not ac-completing))
+             (not ac-completing))
     (let* ((title (ghc-flymake-err-title))
-	   (errs (ghc-flymake-err-list))
-	   (errmsg (pretty-error-message errs)))
+           (errs (ghc-flymake-err-list))
+           (errmsg (pretty-error-message errs)))
       (pos-tip-show errmsg nil nil nil 0)
       ;; (message errmsg)
       )))
 
 (defun pretty-error-message (errs)
   (format "%s"
-	  (substring
-	   (mapconcat
-	    (lambda (x)(ghc-replace-character x ghc-null ghc-newline))
-	    errs "\n")
-	   0 -1)))
+          (substring
+           (mapconcat
+            (lambda (x)(ghc-replace-character x ghc-null ghc-newline))
+            errs "\n")
+           0 -1)))
 
 (defadvice ghc-show-type (around ghc-show-type-around-advice ())
   (lambda ()
     (flet ((message (format-strings args)
-		    (popup-tip (format format-strings args))))
+                    (popup-tip (format format-strings args))))
       (popup-tip "HOGE")
       (ad-do-it)
       )))
@@ -75,12 +75,12 @@
 
 (defvar ghc-flymake-popup-errors-key "\C-c\C-SPC")
 (add-hook 'haskell-mode-hook
-	  (lambda ()
-	    (define-key haskell-mode-map ghc-flymake-popup-errors-key
-	      'ghc-flymake-display-errors-popup)
-	    (setq ghc-flymake-popup-errors-timer-handler
-		  (run-with-idle-timer
-		   1.0 t
-		   #'ghc-flymake-display-errors-popup))))
+          (lambda ()
+            (define-key haskell-mode-map ghc-flymake-popup-errors-key
+              'ghc-flymake-display-errors-popup)
+            (setq ghc-flymake-popup-errors-timer-handler
+                  (run-with-idle-timer
+                   1.0 t
+                   #'ghc-flymake-display-errors-popup))))
 
 (provide 'init.ghc-mode)
