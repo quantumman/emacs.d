f590ce16493429fce75c720a97c30a83de04ccb2
Shoadi Yokoyama <quantumcars@gmail.com>
2012-12-13 23:22:11 +0900

    init.elscreen: Unify indentation

--- init.d/init.elscreen.el
+++ init.d/init.elscreen.el
@@ -2,8 +2,8 @@
 (require 'elscreen)
 (elscreen-start)
 (global-set-key (kbd "C-z C-o")
-		'(lambda () (interactive)
-		   (anything 'anything-c-source-elscreen)))
+                '(lambda () (interactive)
+                   (anything 'anything-c-source-elscreen)))
 (defmacro elscreen-create-automatically (ad-do-it)
   `(if (not (elscreen-one-screen-p))
        ,ad-do-it
@@ -26,16 +26,16 @@
 (defun elscreen-frame-title-update ()
   (when (elscreen-screen-modified-p 'elscreen-frame-title-update)
     (let* ((screen-list (sort (elscreen-get-screen-list) '<))
-	   (screen-to-name-alist (elscreen-get-screen-to-name-alist))
-	   (title (mapconcat
-		   (lambda (screen)
-		     (format "%d%s %s"
-			     screen (elscreen-status-label screen)
-			     (get-alist screen screen-to-name-alist)))
-		   screen-list " ")))
+           (screen-to-name-alist (elscreen-get-screen-to-name-alist))
+           (title (mapconcat
+                   (lambda (screen)
+                     (format "%d%s %s"
+                             screen (elscreen-status-label screen)
+                             (get-alist screen screen-to-name-alist)))
+                   screen-list " ")))
       (if (fboundp 'set-frame-name)
-	  (set-frame-name title)
-	(setq frame-title-format title)))))
+          (set-frame-name title)
+        (setq frame-title-format title)))))
 
 (eval-after-load "elscreen"
   '(add-hook 'elscreen-screen-update-hook 'elscreen-frame-title-update))
