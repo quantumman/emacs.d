80ee75a04f27522f3c17ed4216ea737dcfedb5ac
Shoadi Yokoyama <quantumcars@gmail.com>
2013-01-03 14:39:01 +0900

    init-helm: List candidate files by also spotlight

--- inits/helm/init-helm.el
+++ inits/helm/init-helm.el
@@ -1,6 +1,7 @@
 (require 'helm)
 (require 'helm-command)
 (require 'helm-config)
+(require 'helm-misc)
 
 ;; misc
 
@@ -8,10 +9,33 @@
       helm-input-idle-delay       0.1
       helm-candidate-number-limit 200)
 
+;; helm, source definitions
+
+(defcustom helm-c-sources-buffers
+  '(helm-c-source-ffap-line
+    helm-c-source-ffap-guesser
+    helm-c-source-buffers-list
+    helm-c-source-recentf
+    helm-c-source-bookmarks
+    helm-c-source-file-cache
+    helm-c-source-files-in-current-dir
+    helm-c-source-mac-spotlight)
+  "List of buffers"
+  :type 'list
+  :group 'helm-config)
+
+;; custom commands
+
+(defun helm-find-other-buffers ()
+  "This is helm-for-buffers."
+  (interactive)
+  (helm-other-buffer helm-c-sources-buffers
+                     "open files or buffers by elscreen, bookmark and filename"))
+
 ;; key bindings
 
 (let ((key-and-func
-       `((,(kbd "C-x C-o") helm-for-files)
+       `((,(kbd "C-x C-o") helm-find-other-buffers)
          (,(kbd "C-^")     helm-c-apropos)
          (,(kbd "C-;")     helm-resume)
          (,(kbd "C-s")     helm-occur)
