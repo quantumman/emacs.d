51d1a65595b6bbf74cc700d616af135abaed8d06
Shoadi Yokoyama <quantumcars@gmail.com>
2013-02-18 01:28:48 +0900

    Add my-delete-trailing-whitespace

---
+++ inits/function/my-delete-trailing-whitespace.el
@@ -0,0 +1,17 @@
+(defun my-delete-trailing-whitespace ()
+  "Delete trailing sapces and tabs in current buffer.
+A Line on which the cursor locates and
+lines which contain only single newline character
+are not targeted by this function."
+  (interactive)
+  (let ((cursor-line (line-number-at-pos)))
+    (save-excursion
+      (save-restriction
+        (goto-char (point-max))
+        (while (re-search-backward "[ \t]+\n" nil t)
+          (unless (eq cursor-line (line-number-at-pos))
+            (delete-horizontal-space))
+          )))))
+
+(require 'el-init)
+(el-init:provide)
