11bda0dea4f526399c59659fbffa40936ab5325e
Shoadi Yokoyama <quantumcars@gmail.com>
2013-02-20 01:57:13 +0900

    Update

--- devel/haskell-utils.el
+++ devel/haskell-utils.el
@@ -1,17 +1,53 @@
-(defun ghc-sort-functions-in-module ()
+(defun haskell-sort-functions-in-module ()
   (save-excursion
-    (goto-char (point-max))
-    (while (re-search-backward "\\(^import.+\\)\\((.+)\\)" nil t)
-      (let ((modules (match-string 2)))
-        (replace-match
-         (concat (match-string 1)
-                 (with-temp-buffer
-                   (save-match-data
-                     (insert modules)
-                     (goto-char (point-min))
-                     (replace-regexp ",\\([^ \t]+\\)" ", \\1")
-                     (sort-regexp-fields nil "\\w+" "\\&" (point-min) (point-max))
-                     (buffer-string)))
-                 )))
+    (ghc-goto-module-position)
+    (while (re-search-backward "^import \\([ \t.]\\|\\w\\)+(" nil t)
+      (goto-char (match-end 0))
+      (let ((begin (match-end 0))
+            (end (line-end-position)))
+        (save-match-data
+          (sort-regexp-fields nil "\\w+" "\\&" begin end)))
       (goto-char (match-beginning 0))
       )))
+
+(defun haskell-add-module ()
+  (interactive)
+  (ghc-insert-module)
+  (ghc-sort-lines (haskell-module-begin-point)
+                  (haskell-module-end-point))
+  (haskell-sort-functions-in-module))
+
+(defun haskell-module-begin-point ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-backward "^import" nil t))
+    (point-to-beginnnig-line)))
+
+(defun haskell-module-end-point ()
+  (save-excursion
+    (ghc-goto-module-position)
+    (point)))
+
+(require' el-expectations)
+
+(expectations
+  (desc "haskell-sort-functions-in-module")
+  (expect "import Data.ByteString (append, concat, cons, snoc)"
+    (with-temp-buffer
+      (insert "import Data.ByteString (concat, snoc, append, cons)")
+      (haskell-sort-functions-in-module)
+      (buffer-string)
+      ))
+  (expect "import qualified Network.HTTP as HTTP (HttpMethod(..), StatusCode(..))"
+    (with-temp-buffer
+      (insert "import qualified Network.HTTP as HTTP (StatusCode(..), HttpMethod(..))")
+      (haskell-sort-functions-in-module)
+      (buffer-string)
+      ))
+  (expect "import qualified Network.HTTP as HTTP (HttpMethod(GET, POST), StatusCode(..)"
+    (with-temp-buffer
+      (insert "import qualified Network.HTTP as HTTP (StatusCode(..), HttpMethod(GET, POST))")
+      (haskell-sort-functions-in-module)
+      (buffer-string)
+      ))
+  )
