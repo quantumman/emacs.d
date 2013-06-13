fc32701ac8e16d8b2dd296e8fee618ada5e5ea78
Syoudai Yokoyama <s-yokoyama@timedia.co.jp>
2013-06-13 10:29:20 +0900

    web-mode: Add basic configuraetions

---
+++ inits/web-mode/init-web-mode.el
@@ -0,0 +1,43 @@
+(require 'web-mode)
+
+;;; emacs 23以下の互換
+(when (< emacs-major-version 24)
+  (defalias 'prog-mode 'fundamental-mode))
+
+;;; 適用する拡張子
+(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+
+;;; インデント数
+(defun web-mode-default-indent-depth ()
+  "Hooks for Web mode."
+  (setq web-mode-html-offset   0)
+  (setq web-mode-css-offset    0)
+  (setq web-mode-script-offset 0)
+  (setq web-mode-php-offset    0)
+  (setq web-mode-java-offset   0)
+  (setq web-mode-asp-offset    0))
+(defun web-mode-aspx-indent-depth ()
+  "Hooks for Web mode in aspx file."
+  (setq web-mode-html-offset   0)
+  (setq web-mode-css-offset    0)
+  (setq web-mode-script-offset 0)
+  (setq web-mode-php-offset    0)
+  (setq web-mode-java-offset   0)
+  (setq web-mode-asp-offset    0))
+
+(add-hook 'web-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "<return>") 'newline)
+	    (if (string-equal "aspx" (file-name-extension (buffer-file-name)))
+		(progn
+		  (web-mode-aspx-indent-depth)
+		  (setq indent-tabs-mode t
+			tab-width 4))
+	      (progn
+		(web-mode-default-indent-depth)
+		))))
