77f80a38286aba0f096c34fc410bd41a84209e25
Shoadi Yokoyama <quantumcars@gmail.com>
2013-07-02 01:17:43 +0900

    init.csharp-mode: Remove code to support obsolete functions

--- init.d/init.csharp-mode.el
+++ init.d/init.csharp-mode.el
@@ -3,9 +3,6 @@
 (push '("\\.cs$" . csharp-mode) auto-mode-alist)
 
 (defvar yas/menu-keymap yas/minor-mode-menu)
-(defalias 'yas/snippet-table-fetch 'yas/fetch)
-(defalias 'yas/snippet-table 'yas/snippet-table-get-create)
-(defalias 'yas/menu-keymap-for-mode 'yas/menu-keymap-get-create)
 
 (defun ac-etags-signature (keyword)
   (save-excursion
