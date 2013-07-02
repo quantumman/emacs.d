a72705172261b70b547ef02b34524f3f8877d787
Shoadi Yokoyama <quantumcars@gmail.com>
2013-07-02 23:10:22 +0900

    Manage setting for twittering-mode by el-init

---
+++ inits/sns/init-twittering-mode.el
@@ -0,0 +1,8 @@
+(require 'twittering-mode)
+
+(twittering-icon-mode t)
+(setq twittering-retweet-format "RT @%s: %t")
+(setq twittering-username "_quantumman_")
+
+(require 'el-init)
+(el-init:provide)
