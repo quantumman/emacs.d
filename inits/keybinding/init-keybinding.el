40778f8e315ef3bc1285495a28e2abad64e4e7b1
Shoadi Yokoyama <quantumcars@gmail.com>
2013-07-06 21:07:07 +0900

    init-keybinding: Delete a following char or chars in region

--- inits/keybinding/init-keybinding.el
+++ inits/keybinding/init-keybinding.el
@@ -4,3 +4,17 @@
 (define-key ac-menu-map "\C-n" 'ac-next)
 (define-key ac-menu-map "\C-p" 'ac-previous)
 
+
+(defun delete-chars-region (start end)
+  (interactive)
+  (save-excursion
+    (save-restriction
+      (narrow-to-region start end)
+      (goto-char (point-min))
+      (delete-forward-char (- (point-max) (point-min)))
+      )))
+
+(define-region-key global-map (kbd "\C-d")
+  'delete-chars-region
+  (lambda ()
+    (delete-forward-char 1)))
