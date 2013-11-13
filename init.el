2182cf6cd4edde4ec306ee2ee19ec7c646623f50
Shoadi Yokoyama <quantumcars@gmail.com>
2013-11-14 00:59:36 +0900

    init.el: Fix not to load powershell[-mode]

--- init.el
+++ init.el
@@ -143,11 +143,12 @@
 (require 'egg)
 
 ;;; Powershell
-(require 'powershell)
-(require 'powershell-mode)
-(push '("\\.ps1$" . powershell-mode) auto-mode-alist)
-(add-to-list 'ac-modes 'powershell-mode)
-(setq powershell-indent 4)
+(when windows-p
+  (require 'powershell)
+  (require 'powershell-mode)
+  (push '("\\.ps1$" . powershell-mode) auto-mode-alist)
+  (add-to-list 'ac-modes 'powershell-mode)
+  (setq powershell-indent 4))
 
 ;;;
 (require 'text-adjust)
