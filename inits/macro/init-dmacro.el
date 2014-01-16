40a9caebf25ac47338b15118e05ac5063c8ca0dc
Shoadi Yokoyama <quantumcars@gmail.com>
2014-01-16 22:40:26 +0900

    Add a macro to use after-eval-load easier

--- inits/macro/init-dmacro.el
+++ inits/macro/init-dmacro.el
@@ -2,5 +2,10 @@
 (global-set-key *dmacro-key* 'dmacro-exec)
 (autoload 'dmacro-exec "dmacro" nil t)
 
+(defmacro eval-after-load-1 (file &rest args)
+  (declare (indent 1))
+ `(eval-after-load ,file
+    (quote (progn ,@args))))
+
 (require 'el-init)
 (el-init:provide)
