97b32ab3b7edd85133c8109d04b752a75ae5b3b3
Shoadi Yokoyama <quantumcars@gmail.com>
2014-01-16 22:14:51 +0900

    init.appearance: Adjust whitespaces color

--- init.d/init.appearance.el
+++ init.d/init.appearance.el
@@ -173,10 +173,10 @@
   (set-face-foreground 'whitespace-newline "gray60")
 
   ;; スペースの色を変更
-  (set-face-foreground 'whitespace-hspace "gray80")
-  (set-face-background 'whitespace-hspace "white")
-  (set-face-foreground 'whitespace-space "gray80")
-  (set-face-background 'whitespace-space "white")
+  (set-face-foreground 'whitespace-hspace "gray60")
+  ;; (set-face-background 'whitespace-hspace "white")
+  (set-face-foreground 'whitespace-space "gray60")
+  ;; (set-face-background 'whitespace-space "white")
   (set-face-foreground 'whitespace-tab "grey60")
   (set-face-background 'whitespace-tab nil)
   (set-face-underline 'whitespace-tab "grey80")
