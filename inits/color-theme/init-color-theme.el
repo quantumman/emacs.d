a41e946227caa681c8b43f83e120d7ecc8379527
Shoadi Yokoyama <quantumcars@gmail.com>
2014-05-08 05:32:42 +0900

    init-color-theme: Revert color on region

--- inits/color-theme/init-color-theme.el
+++ inits/color-theme/init-color-theme.el
@@ -6,6 +6,9 @@
 (load-theme 'base16-railscasts t)
 (enable-theme 'base16-railscasts)
 
+(copy-face 'default 'region)
+(invert-face 'region)
+
 ;; (add-hook 'eshell-mode-hook
 ;;           (lambda nil (color-theme-buffer-local 'color-theme-tango (current-buffer))))
 
