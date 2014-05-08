21d5c0fed0d6fb83a34a0631730b999b221b40d9
Shoadi Yokoyama <quantumcars@gmail.com>
2014-05-08 09:12:09 +0900

    init-color: Improve visibility of text color for git-commit-mode

--- inits/color-theme/init-color-theme.el
+++ inits/color-theme/init-color-theme.el
@@ -9,6 +9,10 @@
 (copy-face 'default 'region)
 (invert-face 'region)
 
+;; Use visible foreground color for git commit summary
+;; because "base16-railscasts" theme uses invisible color for git commit summary.
+(copy-face 'git-commit-text-face 'git-commit-summary-face)
+
 ;; (add-hook 'eshell-mode-hook
 ;;           (lambda nil (color-theme-buffer-local 'color-theme-tango (current-buffer))))
 
