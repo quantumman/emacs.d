00f33a7788ad9e0e261ce58eeacd2ea46f41e784
Shodai Yokoyama <quantumcars@gmail.com>
2014-07-02 19:45:42 +0900

    init-keybinding: Jump any position by few key strokes

--- inits/keybinding/init-keybinding.el
+++ inits/keybinding/init-keybinding.el
@@ -55,5 +55,22 @@
 (global-set-key "\C-b" 'backward-word)
 
 
+(require 'ace-jump-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(defun add-keys-to-ace-jump-mode (prefix c &optional mode)
+  (define-key global-map
+    (read-kbd-macro (concat prefix (string c)))
+    `(lambda ()
+       (interactive)
+       (funcall (if (eq ',mode 'word)
+                    #'ace-jump-word-mode
+                  #'ace-jump-char-mode) ,c))))
+(setq mac-option-modifier 'hyper)
+(loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-" c))
+(loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-" c))
+(loop for c from ?0 to ?9 do (add-keys-to-ace-jump-mode "H-M-" c 'word))
+(loop for c from ?a to ?z do (add-keys-to-ace-jump-mode "H-M-" c 'word))
+
+
 (require 'el-init)
 (el-init:provide)
