306cd8b515806d1fff0d6930de86799340cd69a6
Shodai Yokoyama <quantumcars@gmail.com>
2014-07-16 00:09:49 +0900

    init-web-mode: Active web-mode for more files

--- inits/mode/init-web-mode.el
+++ inits/mode/init-web-mode.el
@@ -5,12 +5,16 @@
   (defalias 'prog-mode 'fundamental-mode))
 
 ;;; 適用する拡張子
-(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\"        . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+
 
 ;;; インデント数
 (defun web-mode-default-indent-depth ()
