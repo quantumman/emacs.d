3138506b20f872fc11cfc4e694cdc84d4a8a78f4
Shodai Yokoyama <quantumcars@gmail.com>
2014-12-13 03:29:55 +0900

    init-csharp-mode: Use csharp-mode with omnisharp-mode

--- inits/devel-util/init-csharp-mode.el
+++ inits/devel-util/init-csharp-mode.el
@@ -1,5 +1,10 @@
 (require 'csharp-mode)
+(require 'omnisharp)
 
+(defun csharp-mode-hook-function ()
+  (omnisharp-mode)
+  )
+(add-hook 'csharp-mode-hook 'csharp-mode-hook-function)
 
 (require 'el-init)
 (el-init:provide)
