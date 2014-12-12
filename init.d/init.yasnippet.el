c203a2eac882eed7a3831a07360ee0322bf0ef93
Shodai Yokoyama <quantumcars@gmail.com>
2014-12-13 03:26:31 +0900

    init.yasnippet: Update configuration of yasnippet

--- init.d/init.yasnippet.el
+++ init.d/init.yasnippet.el
@@ -1,10 +1,5 @@
 ;;;; yasnippet
-(require 'yasnippet)
-(yas/load-directory (expand-file-name "~/.emacs.d/snippets"))
-;; (add-hook 'jde-mode-hook 'yas/minor-mode-on)
-(add-hook 'yatex-mode-hook 'yas/minor-mode-on)
-(add-hook 'csharp-mode 'yas/minor-mode-on)
-;; (require 'yasnippet-config)
-;; (yas/setup  (expand-file-name "~/.emacs.d/plugins/yasnippet"))
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+(yas-global-mode 1)
 
 (provide 'init.yasnippet)
