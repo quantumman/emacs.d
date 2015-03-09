a513e73840bd25a2341bbb92b802fdd21e3c27b8
Shodai Yokoyama <quantumcars@gmail.com>
2015-03-10 01:26:24 +0900

    init-keybinding: Remove unused keybindings

--- inits/keybinding/init-keybinding.el
+++ inits/keybinding/init-keybinding.el
@@ -41,24 +41,6 @@
         (delete-char (- (point-max) (point-min)))
         ))))
 
-(defun point-to-top ()
-  "Put point to top line of window."
-  (interactive)
-  (move-to-window-line 0))
-(global-set-key "\C-t" 'point-to-top) ;; C-t = pointer moves to top of window
-
-
-(defun point-to-bottom ()
-  "Put point to bottom line of window."
-  (interactive)
-  (move-to-window-line -1))
-(global-set-key "\C-b" 'point-to-bottom) ;; C-b = pointer moves to bottom of window
-
-
-(global-set-key "\C-f" 'forward-word)
-(global-set-key "\C-b" 'backward-word)
-
-
 (require 'ace-jump-mode)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 (defun add-keys-to-ace-jump-mode (prefix c &optional mode)
