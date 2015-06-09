33e7f707540d78fe3be55537dfd8d6a24cf8b78a
Shodai Yokoyama <quantumcars@gmail.com>
2015-06-10 08:44:12 +0900

    init-typescript-mode: Do not use tss

--- inits/configs/init-typescript-mode.el
+++ inits/configs/init-typescript-mode.el
@@ -1,13 +1,13 @@
 (require 'typescript)
-(require 'tss)
+;; (require 'tss)
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
-(setq tss-popup-help-key "C-:")
-(setq tss-jump-to-definition-key "C->")
-(setq tss-implement-definition-key "C-c i")
+;; (setq tss-popup-help-key "C-:")
+;; (setq tss-jump-to-definition-key "C->")
+;; (setq tss-implement-definition-key "C-c i")
 
-(tss-config-default)
+;; (tss-config-default)
 
 (require 'el-init)
 (el-init-provide)
