8fa9b12aaf4333ca055c714af10de1e4416a2c3a
Shodai Yokoyama <quantumcars@gmail.com>
2015-06-09 10:02:59 +0900

    Add init-typescript-mode

---
+++ inits/configs/init-typescript-mode.el
@@ -0,0 +1,13 @@
+(require 'typescript)
+(require 'tss)
+
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+
+(setq tss-popup-help-key "C-:")
+(setq tss-jump-to-definition-key "C->")
+(setq tss-implement-definition-key "C-c i")
+
+(tss-config-default)
+
+(require 'el-init)
+(el-init-provide)
