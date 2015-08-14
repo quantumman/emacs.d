c01f8c8b2719f88f746faa1abc70990475d4e5af
Shodai Yokoyama <quantumcars@gmail.com>
2015-08-14 12:31:57 +0900

    init-elixir-mode: Configure alchemist mode

--- inits/configs/init-elixir-mode.el
+++ inits/configs/init-elixir-mode.el
@@ -1,10 +1,16 @@
 (require 'elixir-mode)
 (require 'alchemist)
 
-(setq alchemist-project-compile-when-needed t)
+(setq alchemist-project-compile-when-needed t
+      alchemist-mix-command (executable-find "mix")
+      alchemist-iex-program-name (executable-find "iex")
+      alchemist-execute-command (executable-find "elixr")
+      alchemist-compile-command (executable-find "elixirc")
+      )
 
 (defun elixir-mode-hook-function ()
   (setq indent-tabs-mode nil)
+  (alchemist-mode)
   )
 
 (add-to-list 'elixir-mode-hook
