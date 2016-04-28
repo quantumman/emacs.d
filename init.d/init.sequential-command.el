22ce3783f273afa0fd90242654c8adde819dd97b
Shodai Yokoyama <quantumcars@gmail.com>
2016-04-29 00:28:48 +0900

    Update

--- init.d/init.sequential-command.el
+++ init.d/init.sequential-command.el
@@ -3,13 +3,13 @@
 
 ;; head of sentence -> head of line -> beginning of buffer -> departure
 (define-sequential-command seq-home
-  back-to-indentation beginning-of-line
+  back-to-indentation
   beginning-of-buffer seq-return)
 (global-set-key "\C-a" 'seq-home)
 
 ;; end of line -> end of buffer -> departure
 (define-sequential-command seq-epnd
-  end-of-line end-of-buffer seq-return)
+  end-of-buffer seq-return)
 (global-set-key "\C-e" 'seq-end)
 
 (provide 'init.sequential-command)
